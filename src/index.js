c604eb42ccb8dbd2c0ca6fbddba61d1509354999
VladyslavLysenko <Dodog12362@gmail.com>
2022-10-18 14:59:18 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
 
 
-form.addEventListener("submit", onSubmit)
+form.addEventListener("submit",onSubmit)
+
 function onSubmit(e) {
     e.preventDefault();
-    const findImage = e.currentTarget.elements.searchQuery.value;
+    let findImage = e.currentTarget.elements.searchQuery.value.toLowerCase().trim();
+    apiPixabay()
 
-function apiPixabay() {
+function apiPixabay(keyWord) {
 //    pixabay.com/api
     const baseUrl = `https://pixabay.com/api/`;
     const key = `30662426-21982097d0559eebc608a0eec`;
     const baseUrlOptions = `image_type=photo&orientation=horizontal&safesearch=true`;
-    const perPage = `3`
+    const perPage = `9`
     const page = `9`
     
     return fetch(`${baseUrl}?key=${key}&q=${findImage}&${baseUrlOptions}&per_page=${perPage}&page=${page}`).then(response => {
@@ -22,59 +26,47 @@ function apiPixabay() {
             throw new Error('fail')
         }
         return response.json()
+    }).then(data => {
+        // console.log(data.hits)
+        const render = renderMarkUp(data.hits)
+        // console.log(render);
+        gallery.insertAdjacentHTML("beforeend",render )
     })
 }
-   apiPixabay().then(console.log)
+
 }
 
 
-// hello
+ function renderMarkUp(arrPhotos) {
+     return arrPhotos.map(
+      ({
+        largeImageURL,
+        previewURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+         }) =>
+         {
+             return `<div class="gallery__item">
+      <a class="gallery__link" href="${largeImageURL}">
+            <img class="gallery__image" src="${previewURL}" alt="${tags}" />
+         </a>
+         <p class="gallery__text">Likes ${likes}</p>
+         <p class="gallery__text">Views ${views}</p>
+         <p class="gallery__text">Comments ${comments}</p>
+          <p class="gallery__text">Downloads ${downloads}</p>
+    </div>`
+         }).join('');
+     
 
+ }
 
+ 
+let lightbox = new SimpleLightbox('.gallery a', {
+      captionSelector: "img",
+      captionsData: "alt",
+      captionDelay: 250,
+    });
 
-//  function fetchImages(findImage) {
-//     let image = findImage.toLowerCase().trim();
-    
-//     if (!image) {
-//         findImage.innerHTML = "";
-//     }
-//     else {
-//         const resp = fetch(`https://pixabay.com/api/?key=30662426-21982097d0559eebc608a0eec&q=${image}&image_type=photo&orientation =horizontal&safesearch =true&per_page=9&page=1`)
-//         resp.then(response =>{
-//             if (!response.ok) {
-//                   findImage.innerHTML = "";
-//                 Notiflix.Notify.failure("Oops, there is no country with that name")
-//             }
-//             return response.json()   
-//         }) 
-//         .then(data => {
-//             console.log(data);
-//             for (let index = 0; index < data.length; index++) {
-    
-//     const markup = data.reduce((acc, image) =>
-//      acc + 
-//      `<div class="gallery__item">
-//         <a class="gallery__item" href="${image.largeImageURL}">
-//         <img class="gallery__image" src="${image.webformatUR}" alt="${image.tags}" />
-//         </a>
-//     </div>`, "")
-
-//     gallery.insertAdjacentHTML("beforeend", markup)
-//     console.log(markup);
-// }
-
-//         }).catch(err => console.log(err))
-//     }
-        
-//  }
-
-
-
-
-// //  webformatURL - ссылка на маленькое изображение для списка карточек.
-// // largeImageURL - ссылка на большое изображение.
-// // tags - строка с описанием изображения. Подойдет для атрибута alt.
-// // likes - количество лайков.
-// // views - количество просмотров.
-// // comments - количество комментариев.
-// // downloads - количество загрузок.
